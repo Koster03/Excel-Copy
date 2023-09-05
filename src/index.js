ec1207ca4aedf4a04ada9018639b2922a13e0898
Koster03 <pyatkin-kostik@mail.ru>
2023-09-05 12:21:51 +0300

    Finish base framework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './module'
-import './scss/index.scss'
-console.log('Working!!');
+import { Excel } from "./components/excel/Excel";
+import { Formula } from "./components/formula/formula";
+import { Header } from "./components/header/Header";
+import { Table } from "./components/table/Table";
+import { Toolbar } from "./components/toolbar/Toolbar";
+import "./scss/index.scss";
+
+const excel = new Excel("#app", {
+  components: [Header, Toolbar, Formula, Table],
+});
+
+console.log(excel.render());
--- src/module.js
+++
@@ -1,7 +0,0 @@
-console.log("MODUEKR");
-
-async function start() {
-  return await Promise.resolve("async work");
-}
-
-start().then(console.log);
